b00d550c540177cf38f4855a78eb4fbf810c7e90
Saidakbar_Pardaboyev <pardaboyevsaidakbar103@gmail.com>
2024-08-16 13:27:38 +0500

    added some chnages

--- migrations/000001_create_tables.down.sql
+++ migrations/000001_create_tables.down.sql
@@ -1,5 +1,5 @@
+drop table if exists user_locations;
 drop table if exists refresh_tokens;
 drop table if exists workers_of_branches;
 drop table if exists users;
 drop type if exists user_roles; 
-drop table if exists user_locations;
